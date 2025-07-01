253a61c461060a73cd20cfa07a0abee3586f7027
Darshan Vasani <vadanidarshan@gmail.com>
2025-07-01 16:39:48 +0530

    Cart Functionality

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -11,49 +11,54 @@ import Stripe from "stripe";
 
 
 
-const placeOrder = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
+const placeOrder = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if(!cartData[req.body.itemId]){
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId]+=1;
     }
-
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.status(200).json(new ApiResponse(200, null, "Added to Cart"));
+  } catch (error) {
+    return next(new ApiError(500, "Error placing order"));
+  }
 });
 
-const verifyOrder = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
+const verifyOrder = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 1) {
+      cartData[req.body.itemId] -= 1;
+    } else {
+      delete cartData[req.body.itemId];
     }
-
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.status(200).json(new ApiResponse(200, null, "Removed from Cart"));
+  } catch (error) {
+    return next(new ApiError(500, "Error verifying order"));
+  }
 });
 
-const userOrders = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-
+const userOrders = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    return res.status(200).json(new ApiResponse(200, cartData, "Cart fetched successfully"));
+  } catch (error) {
+    return next(new ApiError(500, "Error fetching user orders"));
+  }
 });
 
-const listOrders = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-
+const listOrders = asyncHandler(async(req, res, next) => {
+  return next(new ApiError(501, "List orders not implemented"));
 });
 
-const updateStatus  = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-
+const updateStatus  = asyncHandler(async(req, res, next) => {
+  return next(new ApiError(501, "Update status not implemented"));
 });
 
 
