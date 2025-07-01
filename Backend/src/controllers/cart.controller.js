6da7751f0d97e8d8fbff2b5bb4a660f605c35da3
Darshan Vasani <vadanidarshan@gmail.com>
2025-07-01 16:09:52 +0530

    Cat Functionality

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -7,33 +7,48 @@ import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
 
-const addToCart = asyncHandler(async(req,res) => {
-    try {
-        
-    } catch (error) {
-        
+const addToCart = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
     }
-
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.status(200).json(new ApiResponse(200, null, "Added to Cart"));
+  } catch (error) {
+    return next(new ApiError(500, "Error adding to cart"));
+  }
 });
 
 // Remove Cart
-const removeFromCart = asyncHandler(async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        
+const removeFromCart = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 1) {
+      cartData[req.body.itemId] -= 1;
+    } else {
+      delete cartData[req.body.itemId];
     }
-
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.status(200).json(new ApiResponse(200, null, "Removed from Cart"));
+  } catch (error) {
+    return next(new ApiError(500, "Error removing from cart"));
+  }
 });
 
 // Fetch User Cart Data
-const getCart = asyncHandler(async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-
+const getCart = asyncHandler(async(req, res, next) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    return res.status(200).json(new ApiResponse(200, cartData, "Cart fetched successfully"));
+  } catch (error) {
+    return next(new ApiError(500, "Error fetching cart data"));
+  }
 })
 
 
