f301ebada23f8587060329c2005770f8f00ee36f
dpvasani <vadanidarshan@gmail.com>
2025-07-02 17:26:51 +0530

    Explore Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
+import ExploreMenu from './components/ExploreMenu/ExploreMenu'
 
 const App = () => {
   return (
     <div className='app'>
       <Header />                    
+      <ExploreMenu />
     </div>
   )
 }
